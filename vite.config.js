e7fecb7f86e7444e88d512cbafdb510d95d16901
mobasshir <mobasshirkhan9931@gmail.com>
2026-01-16 00:48:17 +0530

    fix: allow all hosts in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    port: 3000,
+    allowedHosts: ['fshdwdz2fi.us-east-1.awsapprunner.com']
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 8080,
+    allowedHosts: true
+  },
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: true
+  }
 })
